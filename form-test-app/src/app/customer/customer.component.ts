5ceea42653490af3bab55888baf218ef6587c06d
Tanya <tanya.murrow@hobbylobby.com>
2018-12-04 11:24:45 -0600

    Add reactive form, test data, validators

--- form-test-app/src/app/customer/customer.component.ts
+++ form-test-app/src/app/customer/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Customer } from '../customer';
 
 @Component({
@@ -11,14 +11,36 @@ export class CustomerComponent implements OnInit {
 customerForm: FormGroup;
 customer: Customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl('true')
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
+      sendCatalog: true
+    });
+  }
+
+  setNotification(notifyVia: string) {
+    const phoneControl = this.customerForm.get('phone');
+    if(notifyVia == 'text') {
+      phoneControl.setValidators(Validators.required)
+    }
+    else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+  }
+
+  populateTestData() {
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      email: 'jack@torchwood.com',
+      sendCatalog: false
     });
   }
 
